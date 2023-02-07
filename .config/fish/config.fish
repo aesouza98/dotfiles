24c2029e5ddb76d5f80bdc11a8337046cbd0e834
adriano elias <github@nano.slmail.me>
2023-02-06 22:58:21 -0300

    add images

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+function fish_greeting
+#     pokemon-colorscripts -r 1
+end
+end
+source $HOME/.config/fish/aliases
+set -gx ANSIBLE_VAULT_PASSWORD_FILE ~/.config/ansible/vault_pass
+set -gx EDITOR nvim
