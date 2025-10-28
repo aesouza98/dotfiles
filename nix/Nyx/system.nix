4ff696e42d700241d191d908102aaa43a784344d
Adriano Elias <github@nano.slmail.me>
2025-10-28 13:44:35 -0300

    Nix: Fix nvidia syntax

--- nix/Nyx/system.nix
+++ nix/Nyx/system.nix
@@ -20,11 +20,13 @@
   ];
 
   # NVidia
-  hardware.graphics = [
+  hardware.graphics = {
     enable = true;
     enable32Bit = true;
-  ];
+  };
+
   services.xserver.videoDrivers = [ "nvidia" ];
+
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
