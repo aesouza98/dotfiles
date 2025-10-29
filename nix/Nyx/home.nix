fafc40f0096ee6ffbf06534f3d01fb4a2a37c60a
Adriano Elias <github@nano.slmail.me>
2025-10-28 21:15:46 -0300

    Elephant: try to fix it

--- nix/Nyx/home.nix
+++ nix/Nyx/home.nix
@@ -6,7 +6,7 @@
   home.stateVersion = "25.05";
   home.packages = [
     inputs.walker.packages.${pkgs.system}.default
-    inputs.elephant.packages.${pkgs.system}.default
+    inputs.elephant.packages.${pkgs.system}.elephant-with-providers
   ];
 
   home.file = {
