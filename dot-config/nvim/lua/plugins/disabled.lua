0062a4990f6750cfba3363b818f876fb55c0ae54
Adriano Elias <github@nano.slmail.me>
2025-11-04 09:24:01 -0300

    archy

--- dot-config/nvim/lua/plugins/disabled.lua
+++ dot-config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,3 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "mason-org/mason.nvim", enabled = false },
-  { "nvim-treesitter/nvim-treesitter", enabled = false },
 }
--- dot-config/nvim/lua/plugins/treesitter.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- "nvim-treesitter/nvim-treesitter",
-  -- opts = {
-  --   ensure_installed = {},
-  --   auto_install = false,
-  -- },
-}
