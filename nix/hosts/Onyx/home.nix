3a58757a3a58bb7fdf49d26522c1a62e612b9bd8
Adriano Elias De Souza <github@nano.slmail.me>
2025-11-07 15:27:14 -0300

    Nix: env vars refactor -- gemini

--- nix/hosts/Onyx/home.nix
+++ nix/hosts/Onyx/home.nix
@@ -66,6 +66,19 @@ in {
     EDITOR = "nvim";
     XDG_SCREENSHOTS_DIR = "~/Pictures/Screenshots/";
     TERM = "xterm-256color";
+    # Appearance
+    GTK_DEFAULT_COLOR_SCHEME = "prefer-dark";
+    XCURSOR_THEME = "Yaru";
+    XCURSOR_SIZE = "24";
+    # WLR_NO_HARDWARE_CURSORS = "1";
+    # Nvidia
+    GBM_BACKEND = "nvidia-drm";
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    # Steam
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+    # Flake Path
+    NH_FLAKE = "\${HOME}/.dotfiles/nix";
   };
 
   # User PATH
