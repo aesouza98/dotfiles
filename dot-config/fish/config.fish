b7787c31e8adbc9a1f1e5b4dc13b0270b5523304
Adriano Elias <github@nano.slmail.me>
2026-04-05 22:21:27 -0300

    changes and add atuin

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -1,10 +1,13 @@
 function fish_greeting
-    # pfetch
+    pfetch
 end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    atuin init fish | source
     zoxide init fish | source
     source ~/.config/shell/aliases
-    source (starship init fish --print-full-init | psub)
+
+    # source (starship init fish --print-full-init | psub)
 end
